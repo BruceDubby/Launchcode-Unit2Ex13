997fb926901118724fe41dfd69ac7e7f26d5a89e
B Deaz <breonaj@msn.com>
2021-07-21 16:53:19 -0400

    ready to submit

---
+++ CodingEvents/Controllers/EventsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEvents.Controllers
+{
+    public class EventsController : Controller
+    {
+        static private Dictionary<string,string> Events = new Dictionary<string, string>();
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //Events.Add("Strange Loop");
+            //Events.Add("Grace Hopper");
+            //Events.Add("Code with Pride");
+
+            ViewBag.events = Events;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Events/Add")]
+        public IActionResult NewEvent(string name,string description)
+        {
+            Events.Add(name,description);
+            return Redirect("/Events");
+        }
+    }
+}
